dfe625320a524812a001f32ae214aa81f1182f77
glebshevchukk <glebs@stanford.edu>
2021-02-23 23:54:33 -0800

    Changed pub/sub to use zmq

--- pybot/core.py
+++ pybot/core.py
@@ -1,47 +1,62 @@
+'''
+Basic pub/sub, services, actions defined here.
+Use redis as a parameter store and zeromq to do message comms.
+'''
 import redis
 import jsonpickle
+import zmq
 import numpy as np
 from datetime import datetime
 import time
 from pybot.cmn_structs import *
-from pybot.queues import *
 
 
-class RedisBot(object):
+class Bot(object):
     def __init__(self, namespace='bot', **redis_kwargs):
-        self.__db= redis.Redis()
+        self.db= redis.Redis()
         self.namespace=namespace
         self.publish_topics=[]
         self.subscribe_topics=[]
         self.manager_key ='%s:%s' %(namespace, 'manager')
         self.topic_key = '%s:%s' %(namespace, 'topics')
         self.manager_dict=None
-        self.topics={}
+        self.topic_names=list()
 
     def start(self):
         #first check if manager already exists
-        if self.__db.exists(self.manager_key):
-            self.manager_dict = jsonpickle.decode(self.__db.get(self.manager_key))
+        if self.db.exists(self.manager_key):
+            self.manager_dict = jsonpickle.decode(self.db.get(self.manager_key))
             #get all current topics
             self.refresh_topics()
             if self.manager_dict['active'] == False:
                 self.manager_dict['active'] = True
                 dumped_dict = jsonpickle.encode(self.manager_dict)
-                self.__db.set(self.manager_key,dumped_dict)
+                self.db.set(self.manager_key,dumped_dict)
         else:
             start_time = time.time()
             self.manager_dict = {'active':True,'start_time':start_time}
             dumped_dict = jsonpickle.encode(self.manager_dict)
-            self.__db.set(self.manager_key,dumped_dict)
+            self.db.set(self.manager_key,dumped_dict)
 
     def refresh_topics(self):
-        if self.__db.exists(self.topic_key):
-            topic_names = self.__db.smembers(self.topic_key)
-            for topic_name in topic_names:
-                if topic_name not in self.topics:
-                    self.topics[topic_name] = RedisLIFOQueue(self.__db,self.namespace,topic_name)
+        if self.db.exists(self.topic_key):
+            self.topic_names = [elem.decode('utf-8') for elem in self.db.smembers(self.topic_key)]
 
-    def single_publish(self,topics,msgs):
+    def make_topic(self,topic):
+        #check that this topic exists in the list, otherwise append
+        if topic not in self.topic_names:
+            self.db.sadd(self.topic_key, topic)
+        self.refresh_topics()
+
+class Publisher(object):
+    def __init__(self,bot,port="5556"):
+        self.bot=bot
+        self.port=port
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.PUB)
+        self.socket.bind("tcp://*:%s" % self.port)
+        
+    def single_publish(self,topics:list,msgs:list):
         '''
         Publish a single msg to the current topic. Useful when you don't have a fixed rate that you'd like to send
         information at.
@@ -51,11 +66,38 @@ class RedisBot(object):
             return
 
         for topic,msg in zip(topics,msgs):
-            self.make_topic_for_publish(topic)
+            self.bot.make_topic(topic)
+            topic_index = self.bot.topic_names.index(topic)
             #now we want to push to the end of that topic
-            q = self.topics[topic]
             ser_msg = jsonpickle.encode(msg)
-            q.put(ser_msg)
+            self.socket.send_string("%d %s" % (topic_index, ser_msg))
+
+    def rate_publish(self,topics:list,rate,callback):
+        '''
+        Get a msg from the callback and publish responses to the topics at a certain rate.
+        '''
+        for topic in topics:
+            self.bot.make_topic(topic)
+        time_elapse = 1./rate
+        while True:
+            start = time.time()
+            msgs = callback()
+            for topic,msg in zip(topics,msgs):
+                ser_msg = jsonpickle.encode(msg)
+                topic_index = self.bot.topic_names.index(topic)
+                self.socket.send_string("%d %s" % (topic_index, ser_msg))
+                elapsed = time.time() - start
+                wait_time = time_elapse - elapsed
+                if wait_time > 0:
+                    time.sleep(time_elapse - elapsed)  
+    
+class Subscriber(object):
+    def __init__(self,bot,port="5556"):
+        self.bot=bot
+        self.port=port
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.SUB)
+        self.socket.connect("tcp://localhost:%s" % self.port)
 
     def single_subscribe(self,topics):
         '''
@@ -65,51 +107,30 @@ class RedisBot(object):
         #check that this topic exists in the list
         msgs = []
         for topic in topics:
-            if not self.exists_topic(topic):
-                return None
-            if topic not in self.topics.keys():
-                print("The topic you have subscribed to has not been published to yet.")
-                return None
-            q = self.topics[topic]
-            #now we want to push to the end of that topic
-            msg = q.get()
+            self.bot.make_topic(topic)
+            topic_index = self.bot.topic_names.index(topic)
+            self.socket.setsockopt(zmq.SUBSCRIBE, topic_index)
+            string = self.socket.recv()
+            topic, msg = string.split()
             msg = jsonpickle.decode(msg)
             msgs.append(msg)
         return msgs
-
-    def rate_publish(self,topics,rate,callback):
-        '''
-        Get a msg from the callback and publish responses to the topics at a certain rate.
-        '''
-        qs= []
-        for topic in topics:
-            self.make_topic_for_publish(topic)
-            qs.append(self.topics[topic])
-        time_elapse = 1./rate
-        while True:
-            start = time.time()
-            msgs = callback()
-            for q,msg in zip(qs,msgs):
-                ser_msg = jsonpickle.encode(msg)
-                q.put(ser_msg)
-                elapsed = time.time() - start
-                wait_time = time_elapse - elapsed
-                if wait_time > 0:
-                    time.sleep(time_elapse - elapsed)  
-    
     def rate_subscribe(self,topics,rate,callback):
         '''
         Get latest messages from topics at specified rate then process it with callback
         '''
-        qs = []
         for topic in topics:
-            self.exists_topic_for_subscribe(topic)
-            qs.append(self.topics[topic])
+            self.bot.make_topic(topic)
         time_elapse = 1./rate
         while True:
             start = time.time()
-            for q in qs:
-                msg = q.get()
+            for topic in topics:
+                topic_index = self.bot.topic_names.index(topic)
+                self.socket.setsockopt_string(zmq.SUBSCRIBE, str(topic_index))
+                string = self.socket.recv()
+                string= string.decode('utf-8')
+                index,msg= string.split(" ",1)
+
                 msg = jsonpickle.decode(msg)
                 callback(msg)
                 elapsed = time.time() - start
@@ -117,18 +138,11 @@ class RedisBot(object):
                 if wait_time > 0:
                     time.sleep(time_elapse - elapsed)  
 
-    def exists_topic_for_subscribe(self,topic):
-        self.refresh_topics()
-        #check that this topic exists in the list
-        if topic not in self.topics.keys():
-            print("The topic you have subscribed to has not been published to yet.")
-            return False
 
-    def make_topic_for_publish(self,topic):
-        self.refresh_topics()
-        #check that this topic exists in the list, otherwise append
-        if topic not in self.topics.keys():
-            self.__db.sadd(self.topic_key, topic)
-            self.topics[topic]=RedisLIFOQueue(self.__db,self.namespace,topic)
+class ServiceServer(object):
+    def __init__(self,bot):
+        self.bot = bot
 
-
+class ServiceClient(object):
+    def __init__(self,bot):
+        self.bot=bot
--- tests/pubsub.py
+++ tests/pubsub.py
@@ -1,9 +1,9 @@
-from pybot import RedisBot, make_header
+from pybot import Bot, Publisher,Subscriber,make_header
 import random
 import threading
 import numpy as np
 
-topic = ['testing','tester']
+topic = ['testing']
 rate = 1000.
 
 def random_callback():
@@ -13,21 +13,25 @@ def print_callback(msg):
     print(msg)
 
 def test_rate_pub_sub():
-    bot = RedisBot()
+    bot = Bot()
+    pub = Publisher(bot)
+    sub = Subscriber(bot)
     bot.start()
-    pub_thread = threading.Thread(target=bot.rate_publish, args=(topic,rate,random_callback,))
-    sub_thread = threading.Thread(target=bot.rate_subscribe, args=(topic,rate,print_callback,))
+    pub_thread = threading.Thread(target=pub.rate_publish, args=(topic,rate,random_callback,))
+    sub_thread = threading.Thread(target=sub.rate_subscribe, args=(topic,rate,print_callback,))
     pub_thread.start()
     sub_thread.start()
     pub_thread.join()
     sub_thread.join()
 
 def test_single_pub_sub():
-    bot = RedisBot()
+    bot = Bot()
     bot.start()
+    pub = Publisher(bot)
+    sub = Subscriber(bot)
     for i in range(1000):
-        bot.publish(topic,i)
-        msg = bot.subscribe(topic)
+        pub.single_publish(topic,[make_header()])
+        msg = sub.single_subscribe(topic)
         print(msg)
 
 if __name__ == "__main__":
